669a88502815a3da8e8f3c5d1e050c93909a5c40
Alessandro <alessandromasiero158@libero.it>
2024-09-30 16:03:02 +0200

    Added confirmation dialog when deleting a ToDo

--- lib/application/HomeScreen/widget/ToDoList.dart
+++ lib/application/HomeScreen/widget/ToDoList.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo/application/core/widget/alertDialogs.dart';
 import 'package:todo/data/toDoProvider.dart';
 import 'ToDo.dart';
 
@@ -24,11 +25,25 @@ class _ToDoListState extends State<ToDoList>{
     return Expanded(
       child: ListView.builder(
         itemCount: provider.toDoList.length,
-        itemBuilder: (context, index) {
+        itemBuilder: (context, index) {          
           return Dismissible(
             key: UniqueKey(),
             onDismissed: (direction) async {
-              await provider.removeToDo(index);
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return ConfirmationDialog(
+                    title: "Delete To-Do",
+                    message: "Do you really want to Delete this To-Do?",
+                    onConfirm: () async {
+                      await provider.removeToDo(index);
+                    },
+                    onCancel: () {
+                      setState(() {});
+                    },
+                  );
+                },
+              );
             },
             child: ToDo(
               description: provider.toDoList[index]["description"],
--- lib/application/core/widget/alertDialogs.dart
+++ lib/application/core/widget/alertDialogs.dart
@@ -27,6 +27,47 @@ class SimpleAlertDialog extends StatelessWidget {
   }
 }
 
+class ConfirmationDialog extends StatelessWidget {
+  final String title;
+  final String message;
+  final Function onConfirm;
+  final Function onCancel;
+
+  const ConfirmationDialog({
+    super.key,
+    this.title = "",
+    required this.message,
+    required this.onConfirm,
+    this.onCancel = _defaultOnCancel,
+  });
+
+  static void _defaultOnCancel(){}
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      content: Text(message),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('Cancel'),
+          onPressed: () {
+            Navigator.of(context).pop();
+            onCancel();
+          },
+        ),
+        TextButton(
+          child: const Text('Yes'),
+          onPressed: () {
+            Navigator.of(context).pop();
+            onConfirm();
+          },
+        ),
+      ],
+    );
+  }
+}
+
 /*
 showDialog(
   context: context,
